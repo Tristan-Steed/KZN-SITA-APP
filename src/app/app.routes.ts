59f0f7796cb609562db94dc4da789fd8fe0a4616
TPatel2909 <TPatel@datacentrix.co.za>
2025-02-19 10:35:42 +0200

    Update Tas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReviewFormComponent } from './review-form/review-form.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent }
+  { path: '', component: DashboardComponent, title: 'dashboard' },
+  { path: 'Review', component: ReviewFormComponent, title: 'Review'}
 ];
---
+++ src/app/review-form/review-form.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { Form, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-review-form',
+  imports: [],
+  templateUrl: './review-form.component.html',
+  styleUrl: './review-form.component.css'
+})
+
+
+export class ReviewFormComponent {
+
+  reviewForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.reviewForm= this.fb.group({
+      name: ['', Validators.required],
+      supervisor: ['', Validators.required],
+      jobTitle: ['', Validators.required],
+      component:['', Validators.required],
+      dateofReview: ['', Validators.required],
+      salaryLevel: ['', Validators.required],
+      kras: this.fb.array([this.createKRA()]),
+      gafs: this.fb.array([this.createGAF()])
+    });
+  }
+
+  get kras(): FormArray {
+    return this.reviewForm.get('kras') as FormArray;
+  }
+
+  get gafs(): FormArray{
+    return this.reviewForm.get('gafs') as FormArray;
+  }
+
+  createKRA(): FormGroup {
+    return this.fb.group({
+      kra: ['',Validators.required],
+      weighting: ['', Validators.required],
+      ownAssessment: ['', Validators.required],
+      supervisorAssessment: ['', Validators.required]
+    });
+  
+  }
+
+  createGAF(): FormGroup {
+    return this.fb.group({
+      gaf: ['', Validators.required],
+      weighting: ['', Validators.required],
+      ownRating: ['', Validators.required],
+      supervisorRating: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.reviewForm) {
+      console.log('Form Submitted', this.reviewForm.value);
+      alert('Review form submitted successfully');
+    }
+    else {
+      alert('please fill in all required fields');
+    }
+  }
+
+
+
+}
