00afd3659d2f8d2d0cffecc004b7dfe3e34d947c
SamukeloThedeveloper <smsane@datacetrix.co.za>
2025-02-20 11:58:09 +0200

    latest changes by sam

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PersonalDevelopmentPlanComponent } from './personal-development-plan/personal-development-plan.component';
+import { StrategicDashboardComponent } from './strategic-dashboard/strategic-dashboard.component';
+import { StrategicPlanComponent } from './strategic-plan/strategic-plan.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'PersonalDevelopmentPlan', component: PersonalDevelopmentPlanComponent },
+  { path: 'StrategicDashboard', component: StrategicDashboardComponent },
+  { path: 'StrategicPlanComponent', component: StrategicPlanComponent }
 ];
